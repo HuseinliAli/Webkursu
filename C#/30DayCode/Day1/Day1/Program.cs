3abdc25e1923fe9356c38ae79b66ebab9f8a98a7
HuseinliAli <ali.guseynoov@gmail.com>
2022-06-24 14:02:48 +0400

    SomePractice

---
+++ C#/30DayCode/Day1/Day1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+        // Declare second integer, double, and String variables.
+        int ii, sumi;
+        double dd, sumd;
+        string ss, sums;
+        // Read and save an integer, double, and String to your variables.
+        ii=int.Parse(Console.ReadLine());
+        dd=double.Parse(Console.ReadLine());
+        ss=Console.ReadLine();
+        // Print the sum of both integer variables on a new line.
+        sumi=i+ii;
+        Console.WriteLine(sumi);
+        // Print the sum of the double variables on a new line.
+        sumd=d+dd;
+        Console.WriteLine($"{sumd:F1}");
+        // Concatenate and print the String variables on a new line
+        sums=s+ss;
+        Console.WriteLine(sums);
+        // The 's' variable above should be printed first.
+    }
+}
