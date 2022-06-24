7031622f16ad7e3445222ed7ea7585d8d1d1fd7e
HuseinliAli <ali.guseynoov@gmail.com>
2022-06-24 20:26:59 +0400

    Some Practices

---
+++ new course/Strings/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=20,y=32,z=19;
+            bool result1=(x<y)&&(x<z);
+
+            // int positiveAndDouble=int.Parse(Console.ReadLine());
+            // var result=(positiveAndDouble%2==0)&&(positiveAndDouble>0);
+            // System.Console.WriteLine(result);
+            // string email="aliNureli@gmail.ru";
+            // string password="123456";
+            
+            // string userEmail=Console.ReadLine();
+            // string userPassword=Console.ReadLine();
+
+            // var result=((userEmail==email)&&(userPassword==password));
+
+            // System.Console.WriteLine(result);
+            // System.Console.WriteLine("INT 1: ");
+            // int num1=int.Parse(Console.ReadLine());
+            // System.Console.WriteLine("INT 2: ");
+            // int num2=int.Parse(Console.ReadLine());
+
+            // var result=(num1>num2);
+            // System.Console.WriteLine(result);
+            // int x=10,y=4,z=1324;
+            // System.Console.Write("reqem: ");
+            // int a=int.Parse(Console.ReadLine());
+            // // System.Console.Write("reqem:");
+            // // int b=int.Parse(Console.ReadLine());
+            // // int result = (a*b) - (x+y+z);
+            // // bool result =(a%2)==0?true:false;
+            // var result=Math.Pow(x,y);
+            // System.Console.WriteLine(result);
+        }
+    }
+}
