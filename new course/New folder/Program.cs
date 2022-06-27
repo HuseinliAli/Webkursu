9cdf30a2bd9b25074bcd4eed83105acf82a14cea
HuseinliAli <ali.guseynoov@gmail.com>
2022-06-27 20:37:48 +0400

    Some practise

---
+++ new course/New folder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+            int rnd=(new Random()).Next(1,10);
+            int chance=5;
+            while(chance>0){
+                chance--;
+                Console.Write("Please enter which number you want find: ");
+                int estimated=int.Parse(Console.ReadLine());
+                if(estimated==rnd)
+                {
+                    System.Console.WriteLine("You find element.Congratulations");
+                    break;
+                }
+                else if(rnd>estimated)
+                {
+                    System.Console.WriteLine("High");
+                }
+                else if(rnd<estimated)
+                {
+                    System.Console.WriteLine("Low");
+                }
+                
+            }
+            System.Console.WriteLine("Program was excited: "+rnd);
+            System.Console.WriteLine("Your score: "+chance);
+            System.Console.WriteLine("Do you want continue to the game? y-n");
+            char ch=char.Parse(Console.ReadLine());
+            if(ch=='n'||ch=='N'){
+                break;
+            }
+            } while (true);
+        }
+    }
+}
